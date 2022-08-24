18c21cfd87a865f38715cce7d8d0407364ae7bf3
sriharsha annamaneni <sriharsha0806@gmail.com>
2022-08-24 23:33:07 +0530

    Update translate_lan.py

--- translate_lan.py
+++ translate_lan.py
@@ -1,13 +1,20 @@
-from google.cloud import translate
-
 class translate_language:
   @staticmethod
   def translation(string, target_lang="de"):
-    client = translate.TranslationServiceClient()
-    response = client.translate_text(
-      contents = [string],
-      target_language_code=target_lang
-    )
-    for translation in response.translations:
-       return translation.translated_text
+    translate = {
+      'LOW_TEMPERATURE_BREACH' : 'NIEDRIGE_TEMPERATURVERLETZUNG',
+      'LOW_TEMPERATURE_WARNING' : 'WARNUNG_NIEDRIGE_TEMPERATUR',
+      'NORMAL_TEMPERATURE' 'NORMALE_TEMPERATUR':,
+      'HIGH_TEMPERATURE_WARNING' : 'WARNUNG_ZU_HOHER_TEMPERATUR',
+      'HIGH_TEMPERATURE_BREACH' : 'HOHE_TEMPERATURVERLETZUNG',
+      'LOW_SOC_BREACH' : 'NIEDRIGE_SOC_VERLETZUNG',
+      'LOW_SOC_WARNING' : 'WARNUNG_NIEDRIGE_SOC',
+      'NORMAL_SOC' : 'NORMALE_SOC',
+      'HIGH_SOC_WARNING' : 'WARNUNG_ZU_HOHER_SOC',
+      'HIGH_SOC_BREACH' : 'HOCH_SOC_WARNING',
+      'NORMAL_CHARGE_RATE' : 'NORMALE_LADERATE',
+      'CHARGE_RATE_WARNING' : 'LADESTROM_WARNUNG',
+      'CHARGE_RATE_ERROR' : 'LADESTROM_ERROR',
+    }
+    return translate.get(str)
 
