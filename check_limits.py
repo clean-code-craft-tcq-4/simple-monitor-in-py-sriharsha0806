e21b9e593a371908a157593533340c6cad33a378
sriharsha annamaneni <sriharsha0806@gmail.com>
2022-08-24 23:01:31 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,15 +1,32 @@
+# OCP
 from BMS import Temperature, Soc, Charge_rate
-
+from translate_lan import translate_language
 class main:
+   @staticmethod
+    def formatter(string, lan="en"):
+      if lan == "de":
+        print(translate_language.translate(string, destination=lan))
+      else:
+        print(string)
+ 
   @staticmethod
-  def battery_is_ok(temperature, soc, charge_rate):
-    temp_bool = Temperature.health(temperature)
-    soc_bool = Soc.health(soc)
-    charge_rate_bool = Charge_rate.health(charge_rate)
+  def battery_is_ok(temperature, soc, charge_rate, lan="en"):
+    temp_formatter, temp_bool = Temperature.health(temperature)
+    soc_formatter, soc_bool = Soc.health(soc)
+    charge_rate_formatter, charge_rate_bool = Charge_rate.health(charge_rate)
     features = [temp_bool, soc_bool, charge_rate_bool]
-    if not any(features):
+    main.formatter(temp_formatter)
+    main.formatter(soc_formatter)
+    main.formatter(charge_rate_formatter)
+    
+    if all(features):
       return True
     return False
+  
+   
+        
+        
+      
 
   
   
