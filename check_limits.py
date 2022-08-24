6648b3e75b7f8c4bccfdf485472ee7eaca87acf7
sriharsha annamaneni <sriharsha0806@gmail.com>
2022-08-24 14:28:46 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,12 +1,12 @@
-from BMS import Temperature, Soc, Charge
+from BMS import Temperature, Soc, Charge_rate
 
 class main:
   @staticmethod
   def battery_is_ok(temperature, soc, charge_rate):
-    temp_res = Temperature.health(temperature)
-    soc_res = Soc.health(soc)
-    charge_res = Charge.health(charge_rate)
-    if temp_res and soc_res and charge_res:
+    temp_bool = Temperature.health(temperature)
+    soc_bool = Soc.health(soc)
+    charge_bool = Charge_rate.health(charge_rate)
+    if temp_bool and soc_bool and charge_rate_bool:
       return True
     return False
 
