7e0b04e99da69311910942bfa9762b56b35e1860
sriharsha annamaneni <sriharsha0806@gmail.com>
2022-08-24 14:34:28 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -5,7 +5,7 @@ class main:
   def battery_is_ok(temperature, soc, charge_rate):
     temp_bool = Temperature.health(temperature)
     soc_bool = Soc.health(soc)
-    charge_bool = Charge_rate.health(charge_rate)
+    charge_rate_bool = Charge_rate.health(charge_rate)
     features = [temp_bool, soc_bool, charge_rate_bool]
     if not any(features):
       return True
