f8a0561607bad1e2b1d90684ba0d6521736d9311
sriharsha annamaneni <sriharsha0806@gmail.com>
2022-08-25 14:05:52 +0530

    Revert "Feedback"

--- check_limits.py
+++ check_limits.py
@@ -1,18 +1,18 @@
-from BMS import Temperature, Soc, Charge_rate
 
-class main:
-  @staticmethod
-  def battery_is_ok(temperature, soc, charge_rate):
-    temp_bool = Temperature.health(temperature)
-    soc_bool = Soc.health(soc)
-    charge_rate_bool = Charge_rate.health(charge_rate)
-    features = [temp_bool, soc_bool, charge_rate_bool]
-    if not any(features):
-      return True
+def battery_is_ok(temperature, soc, charge_rate):
+  if temperature < 0 or temperature > 45:
+    print('Temperature is out of range!')
+    return False
+  elif soc < 20 or soc > 80:
+    print('State of Charge is out of range!')
+    return False
+  elif charge_rate > 0.8:
+    print('Charge rate is out of range!')
     return False
 
-  
-  
+  return True
 
 
-
+if __name__ == '__main__':
+  assert(battery_is_ok(25, 70, 0.7) is True)
+  assert(battery_is_ok(50, 85, 0) is False)
